8b26d74b2901e682519fd1b01cec3fbd7649178b
Joshua Gager <jlgager@jgcomptech.com>
2017-03-31 20:59:54 -0400

    Accidently marked fields final in DatabaseConnectionInfo
    Signed-off-by: Joshua Gager <jlgager@outlook.com>

--- src/main/java/com/jgcomptech/tools/databasetools/DatabaseConnectionInfo.java
+++ src/main/java/com/jgcomptech/tools/databasetools/DatabaseConnectionInfo.java
@@ -4,8 +4,8 @@ package com.jgcomptech.tools.databasetools;
  * Connection info object to be used to connect to a database
  */
 public class DatabaseConnectionInfo {
-    public final String Path = "";
-    public final String Username = "";
-    public final String Password = "";
+    public String Path = "";
+    public String Username = "";
+    public String Password = "";
     public DatabaseType DBType;
 }

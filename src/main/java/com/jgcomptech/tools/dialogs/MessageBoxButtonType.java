a4b69f53d4b0d736eaaaa560066f1645b469c679
Joshua Gager <jlgager@jgcomptech.com>
2017-04-04 22:40:29 -0400

    Javadoc Updates
    Signed-off-by: Joshua Gager <jlgager@outlook.com>

--- src/main/java/com/jgcomptech/tools/dialogs/MessageBoxButtonType.java
+++ src/main/java/com/jgcomptech/tools/dialogs/MessageBoxButtonType.java
@@ -8,56 +8,56 @@ import javafx.scene.control.ButtonType;
  */
 public class MessageBoxButtonType {
     /**
-     * A pre-defined {@link ButtonType} that displays "Apply" and has a
-     * {@link ButtonBar.ButtonData} of {@link ButtonBar.ButtonData#APPLY}.
+     * A pre-defined {@code ButtonType} that displays "Apply" and has a
+     * {@code ButtonBar.ButtonData} of {@code ButtonBar.ButtonData.APPLY}.
      */
     public static final ButtonType APPLY = ButtonType.APPLY;
 
     /**
-     * A pre-defined {@link ButtonType} that displays "OK" and has a
-     * {@link ButtonBar.ButtonData} of {@link ButtonBar.ButtonData#OK_DONE}.
+     * A pre-defined {@code ButtonType} that displays "OK" and has a
+     * {@code ButtonBar.ButtonData} of {@code ButtonBar.ButtonData.OK_DONE}.
      */
     public static final ButtonType OK = ButtonType.OK;
 
     /**
-     * A pre-defined {@link ButtonType} that displays "Cancel" and has a
-     * {@link ButtonBar.ButtonData} of {@link ButtonBar.ButtonData#CANCEL_CLOSE}.
+     * A pre-defined {@code ButtonType} that displays "Cancel" and has a
+     * {@code ButtonBar.ButtonData} of {@code ButtonBar.ButtonData.CANCEL_CLOSE}.
      */
     public static final ButtonType CANCEL = ButtonType.CANCEL;
 
     /**
-     * A pre-defined {@link ButtonType} that displays "Close" and has a
-     * {@link ButtonBar.ButtonData} of {@link ButtonBar.ButtonData#CANCEL_CLOSE}.
+     * A pre-defined {@code ButtonType} that displays "Close" and has a
+     * {@code ButtonBar.ButtonData} of {@code ButtonBar.ButtonData.CANCEL_CLOSE}.
      */
     public static final ButtonType CLOSE = ButtonType.CLOSE;
 
     /**
-     * A pre-defined {@link ButtonType} that displays "Yes" and has a
-     * {@link ButtonBar.ButtonData} of {@link ButtonBar.ButtonData#YES}.
+     * A pre-defined {@code ButtonType} that displays "Yes" and has a
+     * {@code ButtonBar.ButtonData} of {@code ButtonBar.ButtonData.YES}.
      */
     public static final ButtonType YES = ButtonType.YES;
 
     /**
-     * A pre-defined {@link ButtonType} that displays "No" and has a
-     * {@link ButtonBar.ButtonData} of {@link ButtonBar.ButtonData#NO}.
+     * A pre-defined {@code ButtonType} that displays "No" and has a
+     * {@code ButtonBar.ButtonData} of {@code ButtonBar.ButtonData.NO}.
      */
     public static final ButtonType NO = ButtonType.NO;
 
     /**
-     * A pre-defined {@link ButtonType} that displays "Finish" and has a
-     * {@link ButtonBar.ButtonData} of {@link ButtonBar.ButtonData#FINISH}.
+     * A pre-defined {@code ButtonType} that displays "Finish" and has a
+     * {@code ButtonBar.ButtonData} of {@code ButtonBar.ButtonData.FINISH}.
      */
     public static final ButtonType FINISH = ButtonType.FINISH;
 
     /**
-     * A pre-defined {@link ButtonType} that displays "Next" and has a
-     * {@link ButtonBar.ButtonData} of {@link ButtonBar.ButtonData#NEXT_FORWARD}.
+     * A pre-defined {@code ButtonType} that displays "Next" and has a
+     * {@code ButtonBar.ButtonData} of {@code ButtonBar.ButtonData.NEXT_FORWARD}.
      */
     public static final ButtonType NEXT = ButtonType.NEXT;
 
     /**
-     * A pre-defined {@link ButtonType} that displays "Previous" and has a
-     * {@link ButtonBar.ButtonData} of {@link ButtonBar.ButtonData#BACK_PREVIOUS}.
+     * A pre-defined {@code ButtonType} that displays "Previous" and has a
+     * {@code ButtonBar.ButtonData} of {@code ButtonBar.ButtonData.BACK_PREVIOUS}.
      */
     public static final ButtonType PREVIOUS = ButtonType.PREVIOUS;
 

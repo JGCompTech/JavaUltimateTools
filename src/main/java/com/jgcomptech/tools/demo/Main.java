28c346aaaab0320a20bf73e46373bff70fa47542
Joshua Gager <jlgager@jgcomptech.com>
2017-04-05 18:22:04 -0400

    Updated to Version 1.3.1 and removed lib directory
    Signed-off-by: Joshua Gager <jlgager@outlook.com>

--- src/main/java/com/jgcomptech/tools/demo/Main.java
+++ src/main/java/com/jgcomptech/tools/demo/Main.java
@@ -3,11 +3,17 @@ package com.jgcomptech.tools.demo;
 import com.jgcomptech.tools.ComputerInfo;
 import com.jgcomptech.tools.dialogs.MessageBox;
 import com.jgcomptech.tools.dialogs.MessageBoxIcon;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.security.KeyPair;
 
-public class Main {
+public class Main extends Application {
     private static void Print(String str) { System.out.println(str); }
 
     private static void Print(Boolean str) { System.out.println(str); }
@@ -19,18 +25,25 @@ public class Main {
         System.out.println("Public Key: " + keyPair.getPublic());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) {
         try {
+            final MavenXpp3Reader reader = new MavenXpp3Reader();
+            final Model model = reader.read(new FileReader("pom.xml"));
+
+
             Print("------------------------------------");
-            Print("------Java Ultimate Tools v1.3------");
-            Print("-------Created by J&G CompTech------");
+            Print("------Java Ultimate Tools " + model.getVersion() + "------");
+            Print("-------Created by " + model.getOrganization().getName() + "------");
             Print("------------------------------------");
             Print("Loading Computer Info Please Wait...");
             final ComputerInfo compInfo = new ComputerInfo();
             Print("");
             Print(compInfo.OS.InstallInfo().NameExpandedFromRegistry());
             Print("------------------------------------");
-        } catch(IOException | InterruptedException e) {
+        } catch(XmlPullParserException | IOException | InterruptedException e) {
             MessageBox.show(e.getMessage(), "Error", MessageBoxIcon.ERROR);
         }
     }
